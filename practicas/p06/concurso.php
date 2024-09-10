492be7a58a38f2199d94fee95191ddcdb4d68191
Aleee-98 <alejgoor.98@gmail.com>
2024-09-10 13:40:17 -0600

    Ventana concurso.php

---
+++ practicas/p06/concurso.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- saved from url=(0050)http://52.43.117.247/neo/classroom/tw/concurso.php -->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		
+		<title>Registro Completado</title>
+		<style type="text/css">
+			body {margin: 20px; 
+			background-color: #C4DF9B;
+			font-family: Verdana, Helvetica, sans-serif;
+			font-size: 90%;}
+			h1 {color: #005825;
+			border-bottom: 1px solid #005825;}
+			h2 {font-size: 1.2em;
+			color: #4A0048;}
+		</style>
+	<script src="chrome-extension://obbeehkplijogilkjkpkicnanojdckoi/js/button.js"></script></head>
+	<body>
+		<h1>MUCHAS GRACIAS</h1>
+
+		<p>Gracias por entrar al concurso de Tenis Mike® "Chidos mis Tenis". Hemos recibido la siguiente información de tu registro:</p>
+
+		<h2>Acerca de ti:</h2>
+		<ul>
+			<li><strong>Nombre:</strong> <em></em></li>
+			<li><strong>E-mail:</strong> <em></em></li>
+			<li><strong>Télefono:</strong> <em></em></li>
+		</ul>
+		<p><strong>Tu triste historia:</strong> <em></em></p>
+
+		<h2>Tu diseño de Tenis (si ganas)</h2>
+		<ul>
+			<li><strong>Color:</strong> <em></em></li>
+						<li><strong>Talla:</strong> <em></em></li>
+		</ul>
+		<p>
+		    <a href="http://validator.w3.org/check?uri=referer"><img src="./concurso_files/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88"></a>
+		</p>
+	
+</body></html>
