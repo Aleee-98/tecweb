8b8854c83eb859319663c8569c31ee8557d338a1
Aleee-98 <alejgoor.98@gmail.com>
2024-09-24 20:35:32 -0600

    Practica 9, ejercicio 6

--- practicas/p09/set_producto_v2.php
+++ practicas/p09/set_producto_v2.php
@@ -29,8 +29,15 @@ if ($result->num_rows > 0) {
     echo 'El producto ya existe en la base de datos.';
 } else {
     // Si no existe, lo insertamos
-    $sql = "INSERT INTO productos (id, nombre, marca, modelo, precio, detalles, unidades, imagen, eliminado) 
-            VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
+    /*$sql = "INSERT INTO productos (id, nombre, marca, modelo, precio, detalles, unidades, imagen, eliminado) 
+            VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";*/
+    // Esta nueva query especifica los nombres de las columnas y no incluye valores
+
+    // para las columnas 'id' ni 'eliminado', permitiendo que 'id' sea auto-incremental
+    // y que 'eliminado' utilice su valor predeterminado (0).
+    $sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) 
+    VALUES ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+
 
     if ($link->query($sql)) {
         echo 'Producto insertado con éxito.<br/>';
@@ -43,7 +50,7 @@ if ($result->num_rows > 0) {
         echo 'Detalles: ' . $detalles . '<br/>';
         echo 'Unidades: ' . $unidades . '<br/>';
         echo 'Imagen: ' . $imagen . '<br/>';
-        echo 'Eliminado: 0<br/>';
+        echo 'Eliminado: 0<br/>';//aquiiiiiii :)
     } else {
         echo 'El Producto no pudo ser insertado =(';
     }
