3d0be24e8f567ab4813aeb90a18f0c7ea3380ed9
Aleee-98 <alejgoor.98@gmail.com>
2024-09-27 20:12:18 -0600

    Actividad 2: ejercicios de js03 Modificacion de main e index

--- ACTIVIDAD/Actividad 2/05-ejercicio_js/src/main.js
+++ ACTIVIDAD/Actividad 2/05-ejercicio_js/src/main.js
@@ -73,3 +73,65 @@ function mayor(){
         div.innerHTML = '</h3>El mayor es: '+ num2+'</h3>';    
     }
 }
+
+function calificaciones() {
+    var nota1, nota2, nota3;
+    
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+        
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+    
+    var pro = (nota1 + nota2 + nota3) / 3;    
+    var div = document.getElementById('calificaciones');
+        
+    if (pro >= 7) {
+        div.innerHTML = '<h3>Aprobado</h3>';
+    } else if (pro >= 4) {
+        div.innerHTML = '<h3>Regular</h3>';
+    } else {
+        div.innerHTML = '<h3>Reprobado</h3>';
+    }
+}
+
+function numeroLetras(){
+    var valor = prompt('Ingresar un valor comprendido entre 1 y 5:', '' );
+    //Convertimos a entero
+    valor = parseInt(valor);
+    var div = document.getElementById('numeroLetra');
+    switch (valor) {
+    case 1: div.innerHTML = '<h3>uno</h3>';
+            break;
+    case 2: div.innerHTML = '<h3>dos</h3>';
+            break;
+    case 3: div.innerHTML = '<h3>tres</h3>';
+            break;
+    case 4: div.innerHTML = '<h3>cuatro</h3>';
+            break;
+    case 5: div.innerHTML = '<h3>cinco</h3>';
+            break;
+    default:div.innerHTML = '<h3>Debe ingresar un valor entre 1 y 5</h3>';
+    }
+}
+
+function color() {
+    var col = prompt('Ingresa el color con que quieres pintar el fondo de la ventana (rojo, verde, azul)', '');
+
+    switch (col.toLowerCase()) {  // Convertir la entrada a minúsculas para ser más flexible
+        case 'rojo':
+            document.body.style.backgroundColor = '#ff0000'; // Cambiar el color del fondo
+            break;
+        case 'verde':
+            document.body.style.backgroundColor = '#00ff00';
+            break;
+        case 'azul':
+            document.body.style.backgroundColor = '#0000ff';
+            break;
+        default:
+            alert('Color no válido. Por favor, ingresa "rojo", "verde" o "azul".');
+            break;
+    }
+}
