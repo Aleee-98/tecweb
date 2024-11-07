08c7ed67357dae2664ddaba99f2c9053b1ee15ba
Aleee-98 <alejgoor.98@gmail.com>
2024-11-06 20:53:20 -0600

    Ejemplos php

---
+++ ejemplos/PHP/02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 2</title>
+</head>
+<body>
+    <?php
+    require_once __DIR__.'/Menu.php';
+    $menu1 = new Menu;
+    
+    $menu1->cargar_opcion('http://facebook.com','Facebook');
+    $menu1->cargar_opcion('http://instagram.com','Instagram');
+    $menu1->cargar_opcion('http://x.com','X');
+
+    $menu1->mostrar();
+    
+    ?>
+</body>
+</html>
